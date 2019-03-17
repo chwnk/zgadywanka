bc060d02793e9802e80c611a4f0bbbc2ea4378e4
chwnk <48645500+chwnk@users.noreply.github.com>
2019-03-17 16:01:00 +0100

    Lab01

---
+++ Lab01/HelloConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program na powitanie.");
+
+            Console.Write("Podaj imie: ");
+            string imie;
+            imie = Console.ReadLine();
+
+            Console.Write("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko);
+            Console.ReadKey();
+        }
+    }
+}
